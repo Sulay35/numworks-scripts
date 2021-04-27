6eee6ac204aa8bdab019e530599a0121f7d319dd
Sulaymane <50274801+Sulay35@users.noreply.github.com>
2021-04-27 16:15:10 +0200

    Update integrale.py

--- integrale.py
+++ integrale.py
@@ -8,22 +8,28 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 import numpy as np
 
-def aproxIntegrale(n,p):
-    fn = lambda x: (x**n)*(exp(-x))
+def aproxIntegrale(a,b,p):
+    f = lambda x: x
     I = 0
+    coef = (b-a)/p
     
-    x1 = np.arange(0, 1+1/p, 1/p)
+    x1 = np.arange(a, b+coef, coef)
     fig, ax = plt.subplots()
     
-    plt.plot(x1, fn(x1), 'bo')
-    ax.plot(x1, fn(x1), c="cyan")
+    plt.plot(x1, f(x1), 'bo')
+    ax.plot(x1, f(x1), c="cyan")
+    
+   
     
     for k in range(p):
-        x = k/p
-        ax.add_patch(Rectangle((x, 0), 1/p, fn((k+1)/p),color="red", alpha=0.5))
-        I = I + fn(x)*(1/p)
+        x = k*coef
+        
+        
+        ax.add_patch(Rectangle((k*coef, 0), coef, f(k*coef),  color="red", alpha=0.5))
+        
+        I = I + f(x)*coef
     
-    ax.text(0.3,exp(-1)/2,"I ≃ "+str(round(I,3)), fontsize=20)
+    ax.text(b/2, f(b)/2,"I ≃ "+str(round(I,3)), fontsize=20)
     
     plt.title("Approximation d'une intégrale : méthode des rectangles")
     ax.set_xlabel("x")
@@ -31,3 +37,5 @@ def aproxIntegrale(n,p):
     plt.show()
     
     return I
+
+aproxIntegrale(0,25,100)
